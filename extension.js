37ff17ec1fc50af8fa736670dce2ef7916ab26c2
Mr Pixel <mr-pixel-dev@users.noreply.github.com>
2025-11-20 10:20:47 +0000

    Initialize timeout ID in constructor

--- extension.js
+++ extension.js
@@ -30,7 +30,6 @@ class ClassicAppSwitcher extends PanelMenu.Button {
         this._workspaceId = null;
         this._trackerId = null;
         this._settingsChangedId = null;
-<<<<<<< HEAD
         
         // Initialize separate timeout IDs for each function
         this._activateTimeoutId = null;
@@ -38,9 +37,6 @@ class ClassicAppSwitcher extends PanelMenu.Button {
         this._showAllTimeoutId = null;
         this._hideCurrentAppTimeoutId = null;
         this._hideOthersTimeoutId = null;
-=======
-        this._timeoutId = null;
->>>>>>> d146d4823664e0a084b9e1104b24b93543b2a2db
 
         this._buildUI();
         this._buildMenu();
